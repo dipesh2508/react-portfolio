c08732e6a25e6c9f40fc551b6f3801c4d2143357
Dipesh Ranjan <sinhadipesh25@gmail.com>
2023-06-14 01:28:47 +0530

    changes in packages.json

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,57 @@
 import React from 'react'
 import './experience.css'
+import {BsPatchCheckFill} from 'react-icons/bs'
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>What Skills I have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__frontend">
+
+        {/* front end technologies */}
+            <h3>Frontend Development</h3>
+            <div className="experience__content">
+              <article className="experience__details">
+                <BsPatchCheckFill />
+                <h4>HTML</h4>
+                <small className='text-light'>Experienced</small>
+              </article>
+
+              <article className="experience__details">
+                <BsPatchCheckFill />
+                <h4>CSS</h4>
+                <small className='text-light'>Intermediate</small>
+              </article>
+
+              <article className="experience__details">
+                <BsPatchCheckFill />
+                <h4>Javascript</h4>
+                <small className='text-light'>Experienced</small>
+              </article>
+
+              <article className="experience__details">
+                <BsPatchCheckFill />
+                <h4>Bootstrap</h4>
+                <small className='text-light'>Experienced</small>
+              </article>
+
+              <article className="experience__details">
+                <BsPatchCheckFill />
+                <h4>React JS</h4>
+                <small className='text-light'>Beginner</small>
+              </article>  
+            </div>
+        </div>
+
+        {/* backend technologies */}
+        <div className="experience__backend">
+
+        </div>
+      </div>
+    </section>
   )
 }
 
