1d5d453ae3eb38136eb9b8a5381d21555e771862
Dipesh Ranjan <sinhadipesh25@gmail.com>
2023-06-15 22:56:09 +0530

    minor additions

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,9 +8,9 @@ import {AiOutlineTwitter} from 'react-icons/ai'
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className='footer__logo'>Dipesh Ranjan</a>
+      <a href="/#" className='footer__logo'>Dipesh Ranjan</a>
       <ul className="permalinks">
-        <li><a href="#">Home</a></li>
+        <li><a href="/#">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#experience">Experience</a></li>
         <li><a href="#services">Services</a></li>
@@ -20,11 +20,11 @@ const Footer = () => {
       </ul>
 
       <div className="footer__socials">
-      <a href='https://www.linkedin.com/in/dipesh-ranjan/' target='_blank'><BsLinkedin /></a>
-        <a href='https://github.com/dipesh2508' target='_blank'><FaGithub /></a>
-        <a href='https://www.instagram.com/dipesh_ranjan07/' target='_blank'><FaInstagram /> </a>
-        <a href="https://www.facebook.com/dipesh.ranjan.007/" target='_blank'><BsFacebook /></a>
-        <a href="https://twitter.com/DipeshRanjan12" target='_blank'><AiOutlineTwitter /></a>
+      <a href='https://www.linkedin.com/in/dipesh-ranjan/' target='_blank' rel="noopener noreferrer"><BsLinkedin /></a>
+        <a href='https://github.com/dipesh2508' target='_blank' rel="noopener noreferrer"><FaGithub /></a>
+        <a href='https://www.instagram.com/dipesh_ranjan07/' target='_blank' rel="noopener noreferrer"><FaInstagram /> </a>
+        <a href="https://www.facebook.com/dipesh.ranjan.007/" target='_blank' rel="noopener noreferrer"><BsFacebook /></a>
+        <a href="https://twitter.com/DipeshRanjan12" target='_blank' rel="noopener noreferrer"><AiOutlineTwitter /></a>
       </div>
 
       <div className="footer__copyright">
