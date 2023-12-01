8877f80170c854662e05ef4a86fe352de9b0010c
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2023-12-02 02:26:47 +0530

    updated portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,8 +4,8 @@ import HealthOptima from "../../assets/images/HealthOptima.png";
 import IMG1 from "../../assets/images/img1.png";
 import IMG2 from "../../assets/images/img2.png";
 import IMG3 from "../../assets/images/img3.png";
-import IMG5 from "../../assets/images/img5.png";
-import IMG6 from "../../assets/images/img6.png";
+import ReactQuiz from "../../assets/images/ReactQuiz.png";
+import Chitksa from "../../assets/images/Chikitsa.png";
 
 const data = [
   {
@@ -16,11 +16,25 @@ const data = [
     demo: "https://www.healthoptima.in/",
   },
   {
+    id: 6,
+    image: Chitksa,
+    title: "Chikitsa Salah",
+    github: "https://github.com/dipesh2508/Chikitsa-Salah",
+    demo: "https://chikitsa-salah-y6bc.vercel.app/",
+  },
+  {
     id: 2,
     image: IMG1,
     title: "Chat App",
     github: "https://github.com/dipesh2508/chat-app",
     demo: "",
+  },
+  {
+    id: 5,
+    image: ReactQuiz,
+    title: "Quiz App",
+    github: "https://github.com/dipesh2508/Quiz-App",
+    demo: "https://dipesh2508.github.io/Quiz-App/",
   },
   {
     id: 3,
@@ -37,20 +51,8 @@ const data = [
     demo: "https://dipesh2508.github.io/sign-page/",
   },
 
-  {
-    id: 5,
-    image: IMG5,
-    title: "Quiz App",
-    github: "https://github.com/dipesh2508/React-Quiz",
-    demo: "https://dipesh2508.github.io/React-Quiz/",
-  },
-  {
-    id: 6,
-    image: IMG6,
-    title: "Stopwatch",
-    github: "https://github.com/dipesh2508/stopwatch",
-    demo: "https://dipesh2508.github.io/stopwatch/",
-  },
+  
+  
 ];
 
 const Portfolio = () => {
